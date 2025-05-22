9e319fd0e9eadf41968de793ab54eada21e88950
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-05-21 23:03:49 -0300

    elements were added to WidgetPage

--- src/test/java/pages/WidgetPage.java
+++ src/test/java/pages/WidgetPage.java
@@ -8,6 +8,32 @@ import org.openqa.selenium.support.PageFactory;
 public class WidgetPage {
     private WebDriver driver;
 
+    @FindBy(xpath = "//*[text()='Accordian']")
+    private WebElement accordianOption;
+
+    @FindBy(xpath = "//*[text()='Auto Complete']")
+    private WebElement autoCompleteOption;
+
+    @FindBy(xpath = "//*[text()='Date Picker']")
+    private WebElement datePickerOption;
+
+    @FindBy(xpath = "//*[text()='Slider']")
+    private WebElement sliderOption;
+
+    @FindBy(xpath = "//*[text()='Progress Bar']")
+    private WebElement progressBarOption;
+
+    @FindBy(xpath = "//*[text()='Tabs']")
+    private WebElement tabsOption;
+
+    @FindBy(xpath = "//*[text()='Tool Tips']")
+    private WebElement toolTipOption;
+
+    @FindBy(xpath = "//*[text()='Menu']")
+    private WebElement menuOption;
+
+    @FindBy(xpath = "//*[text()='Select Menu']")
+    private WebElement selectMenuOption;
 
     // Constructor
     public WidgetPage(WebDriver driver) {
