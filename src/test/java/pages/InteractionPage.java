d9d50b13ba4ea663e3ea98ce8dce3ae694e5186c
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-05-22 23:55:58 -0300

    Elements were added to InteractionPage

--- src/test/java/pages/InteractionPage.java
+++ src/test/java/pages/InteractionPage.java
@@ -25,6 +25,51 @@ public class InteractionPage {
     @FindBy(xpath = "//*[text()='Dragabble']")
     private WebElement dragableOption;
 
+
+    // Draggable Options Tabs
+    @FindBy(id = "draggableExample-tab-simple")
+    private WebElement simpleOption;
+
+    @FindBy(id = "draggableExample-tab-axisRestriction")
+    private WebElement axisOption;
+
+    @FindBy(id = "draggableExample-tab-containerRestriction")
+    private WebElement containerOption;
+
+    @FindBy(id = "draggableExample-tab-cursorStyle")
+    private WebElement cursorOption;
+
+    // Simple Drag
+    @FindBy(id = "dragBox")
+    private WebElement dragBox;
+
+    // Axis Restricted
+    @FindBy(id = "restrictedX")
+    private WebElement dragBoxX;
+
+    @FindBy(id = "restrictedY")
+    private WebElement dragBoxY;
+
+    // Container Restricted
+    @FindBy(xpath = "//*[text()=\"I'm contained within the box\"]")
+    private WebElement elementD1;
+
+    @FindBy(xpath = "//*[text()=\"I'm contained within my parent\"]")
+    private WebElement elementD2;
+
+    @FindBy(id = "containmentWrapper")
+    private WebElement containerWrapper;
+
+    // Cursor Style
+    @FindBy(id = "cursorCenter")
+    private WebElement center;
+
+    @FindBy(id = "cursorTopLeft")
+    private WebElement topLeft;
+
+    @FindBy(id = "cursorBottom")
+    private WebElement bottom;
+
     // Constructor
     public InteractionPage(WebDriver driver) {
         this.driver = driver;
