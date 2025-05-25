33f2096ed9c9a9c88da8023a02a7e8ee00370daf
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-05-24 22:28:46 -0300

    HomePage was added to pages folder

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,76 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * HomePage class for Selenium using @FindBy annotations.
+ */
+public class HomePage {
+
+    private WebDriver driver;
+
+    @FindBy(tagName = "h1")
+    private WebElement title;
+
+    @FindBy(xpath = "//*[text()='Elements']")
+    private WebElement elementsCard;
+
+    @FindBy(xpath = "//*[text()='Forms']")
+    private WebElement formsCard;
+
+    @FindBy(xpath = "//*[text()='Alerts, Frame & Windows']")
+    private WebElement alertsFramesCard;
+
+    @FindBy(xpath = "//*[text()='Widgets']")
+    private WebElement widgetsCard;
+
+    @FindBy(xpath = "//*[text()='Interactions']")
+    private WebElement interactionsCard;
+
+    @FindBy(xpath = "//*[text()='Book Store Application']")
+    private WebElement bookStoreCard;
+
+    private static final String BASE_URL = "https://your-base-url.com"; // Reemplaza con el valor real
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goTo() {
+        driver.get(BASE_URL);
+    }
+
+    // Puedes agregar getters si necesitas acceder a los elementos desde los tests
+
+    public WebElement getTitle() {
+        return title;
+    }
+
+    public WebElement getElementsCard() {
+        return elementsCard;
+    }
+
+    public WebElement getFormsCard() {
+        return formsCard;
+    }
+
+    public WebElement getAlertsFramesCard() {
+        return alertsFramesCard;
+    }
+
+    public WebElement getWidgetsCard() {
+        return widgetsCard;
+    }
+
+    public WebElement getInteractionsCard() {
+        return interactionsCard;
+    }
+
+    public WebElement getBookStoreCard() {
+        return bookStoreCard;
+    }
+}
